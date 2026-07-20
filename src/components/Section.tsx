4cf3898f78cf83e77145ae66043a880b19d4f312
Mohammad Ali Haidar <mohammadali.hd3@gmail.com>
2026-07-20 15:20:47 +0200

    Fixed issue: Row numbers are mixed up due to removing/adding mechanisms by indexing a row to find its number

--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -5,7 +5,7 @@ function Section(section: Section_Type){
     // The Section component visualizes the name of the section followed by its Rows
     return <div key={section.name} ><h2>{section.name}</h2>
             {section.rows.map(row =>
-                <Row key={row.row_number} row_number={row.row_number} input="" stitches={row.stitches} finished={row.finished} final_stitch_count={row.final_stitch_count}/>
+                <Row key={row.row_number} row_number={section.rows.findIndex((r) => { return (r == row)})} input="" stitches={row.stitches} finished={row.finished} final_stitch_count={row.final_stitch_count}/>
             )}
            </div>
 }
