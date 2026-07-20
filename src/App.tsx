a5464a8755ccb145c3046a819d48ae29bb7ef887
Mohammad Ali Haidar <mohammadali.hd3@gmail.com>
2026-07-20 20:27:38 +0200

    Fixed Issue: Exporting as PDF excludes input fields

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,7 @@ function App() {
   }
   // The main app component
   return (
-    <div className="grid grid-cols-1 gap-4 bg-[#F3F3F1] mr-auto ml-auto text-center w-280 mt-20 mb-20 shadow-lg shadow-black">
+    <div className="grid grid-cols-1 gap-4 bg-[#F3F3F1] mr-auto ml-auto text-center mt-20 mb-20 shadow-lg shadow-black w-full max-w-280 mx-auto px-4 py-0">
       <img src={logo} className="w-160 mr-auto ml-auto pt-10 pb-10"></img>
       {/*The following select tag allows the user to pick between their saved patterns*/}
       <div className="mt-10 mb-1 mr-auto ml-auto w-80 border-2 border-black-500 rounded-3xl shadow-md shadow-black">
@@ -58,8 +58,8 @@ function App() {
         {!section.rows[section.rows.length - 1]?.finished?
         // Buttons to allow adding and removing rows from a section
           <>
-          <input type="button" className="transition-transform duration-300 hover:scale-110 mr-5 border-2 border-black-500 rounded-full text-center mt-2 text-2xl w-13 h-13 rounded-full" value="+" onClick={addRow}></input>
-          <input type="button" className="transition-transform duration-300 hover:scale-110  border-2 border-black-500 rounded-full mt-2 font-bold text-2xl w-13 h-13 rounded-full" value="-" onClick={removeRow}></input>
+          <input type="button" data-html2canvas-ignore className="transition-transform duration-300 hover:scale-110 mr-5 border-2 border-black-500 rounded-full text-center mt-2 text-2xl w-13 h-13 rounded-full" value="+" onClick={addRow}></input>
+          <input type="button" data-html2canvas-ignore className="transition-transform duration-300 hover:scale-110  border-2 border-black-500 rounded-full mt-2 font-bold text-2xl w-13 h-13 rounded-full" value="-" onClick={removeRow}></input>
           </> : <></>
         }
         </>
@@ -139,12 +139,13 @@ function App() {
       // The final row's input is finalized
       var last_section = currentPattern.sections[currentPattern.sections.length - 1]
       var last_row_of_last_section = last_section.rows[last_section.rows.length - 1] ?? {row_number: 1, stitches: "", input:"", 
-      finished: true, img: "", final_stitch_count: 0}
+      finished: true, img: undefined, final_stitch_count: 0}
       last_row_of_last_section = {row_number: last_row_of_last_section.row_number, stitches: last_row_of_last_section.stitches, input:last_row_of_last_section.input, 
       finished: true, img: last_row_of_last_section.img, final_stitch_count: last_row_of_last_section.final_stitch_count}
       var last_section_rows = last_section.rows
       last_section_rows.pop()
       last_section_rows = [...last_section_rows, last_row_of_last_section]
+      last_section = {"name": last_section.name, "rows": last_section_rows}
       // An empty section is created with the chosen name
       const new_empty_section = {name: new_section_name, rows: [{row_number: 1, stitches: "", input:"", img: undefined, finished: false, final_stitch_count: 0}]}
       // The last section is finalized
--- src/components/Row.tsx
+++ src/components/Row.tsx
@@ -3,13 +3,13 @@ import type { Row_Type } from "../types/row.ts"
 function Row(row: Row_Type){
     return <div className="text-2xl">Row {row.row_number}: {" "}
     {/* If a row is unfinished, it is rendered as an input box allowing the user to fill it with the corresponding stitches */}
-        {!row.finished ? <><input className="border-2 border-black-500 rounded-full text-center mt-2" type="text" id="latest_row"></input>
-        <input className="border-2 border-black-500 rounded-full text-center mt-2" id="final_stitch_count" placeholder="Final Stitch Count"></input>
-        <input type="number" className="border-2 border-black-500 rounded-full text-center mt-2" id="row_image_input" placeholder="Image URL (optional)"></input></> : 
+        {!row.finished ? <><input data-html2canvas-ignore className="border-2 border-black-500 rounded-full text-center mt-2" type="text" id="latest_row"></input>
+        <input data-html2canvas-ignore className="border-2 border-black-500 rounded-full text-center mt-2" id="final_stitch_count" placeholder="Final Stitch Count"></input>
+        <input data-html2canvas-ignore type="number" className="border-2 border-black-500 rounded-full text-center mt-2" id="row_image_input" placeholder="Image URL (optional)"></input></> : 
         <>
         {/* If a row is finished, its stitches and image if added are rendered */}
         {row.stitches?.toString() + "  [" + row.final_stitch_count + "]"}
-        {!(row.img != undefined) ? <img className="w-90 h-90 ml-auto mr-auto" src={row.img}></img> : <div></div>}
+        {!(row.img != undefined || row.img != "") ? <img className="w-90 h-90 ml-auto mr-auto" src={row.img}></img> : <div></div>}
         </>
         }
     </div>
